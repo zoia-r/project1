d4c6b4051d9c426ab47db7bc099b6db1fe2c7020
zoia-r <94369327+zoia-r@users.noreply.github.com>
2021-12-05 21:13:08 +0200

    Add files via upload

---
+++ HT_5/6.py
@@ -0,0 +1,34 @@
+"""
+6. Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
+   P.S. Повинен вертатись генератор.
+   P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
+"""
+
+
+def range_new(start, finish=0, step=1):
+    if finish == 0:
+        finish = start
+        start = 0
+    if step ==0:
+        raise ValueError()
+    if step>0:
+        while start < finish:
+            yield start
+            start += step
+    else:
+        while start > finish:
+            yield start
+            start += step
+
+
+
+for i in range_new(2):
+    print(i)
+
+
+for i in range_new(1, 10):
+    print(i)
+
+
+for i in range_new(10, 1, -2):
+    print(i)
